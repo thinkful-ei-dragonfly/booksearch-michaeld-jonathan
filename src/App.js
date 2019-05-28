570fedda1300b025d9a9690a313190935e8cb5bf
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-28 12:06:05 -0400

    created basic app.js, static Header, search form. make FilterMenu and BookList next

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SearchForm from "./components/SearchForm/SearchForm";
+import FilterMenu from "./components/FilterMenu/FilterMenu";
+import BookList from "./components/BookList/BookList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <SearchForm />
+          {/* <FilterMenu />
+          <BookList /> */}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SearchForm() {
+  return (
+    <form>
+      <p>
+        Search: <input type="text" />
+        <button type="submit">Submit</button>
+      </p>
+    </form>
+  );
+}
+
+export default SearchForm;
