dd5272eb9803b50a0ecfc5b20c47f450399b797b
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-28 15:43:48 -0500

    Finished Book component

--- src/components/BookList/Book/Book.js
+++ src/components/BookList/Book/Book.js
@@ -2,12 +2,26 @@ import React from 'react'
 
 function Book (props){
   let book = props.books.map(item =>{
-    return(<li>
-      <h2>{item.volumeInfo.title}</h2>
-      <p>Author(s): {item.volumeInfo.authors}</p>
-      <p>Price: {item.saleInfo.listPrice}</p>
-    </li>
-)
+    if(item.saleInfo.saleability === "FOR_SALE"){
+      return(<li>
+        <h2>{item.volumeInfo.title}</h2>
+        <img src={item.volumeInfo.imageLinks.smallThumbnail} alt="book-preview"></img>
+        <p>Author: {item.volumeInfo.authors}</p>
+        <p>Price: {item.saleInfo.listPrice.amount}</p>
+        <p>{item.volumeInfo.description}</p>
+      </li>)
+    }
+    else{
+      return(<li>
+        <h2>{item.volumeInfo.title}</h2>
+        <img src={item.volumeInfo.imageLinks.smallThumbnail} alt="book-preview"></img>
+        <p>Author: {item.volumeInfo.authors}</p>
+        <p>Price: Not For Sale</p>
+        <p>{item.volumeInfo.description}</p>
+      </li>)
+      }
+    
+
   })
   
   return(
